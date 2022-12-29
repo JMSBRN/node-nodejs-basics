5d68b688c538b0b7d834c7ba0e057c56240f5b7b
JMSBRN <zakhavai@gmail.com>
2022-12-29 14:25:10 +0300

    fix: upd_task_1_rename_file

--- src/fs/files/modules.js
+++ src/fs/files/modules.js
@@ -1,5 +1,12 @@
-import path from "path";
-import { existsSync, appendFile, readdirSync, copyFile, mkdir } from "fs";
+import path, { join } from "path";
+import {
+  existsSync,
+  appendFile,
+  readdirSync,
+  copyFile,
+  mkdir,
+  renameSync,
+} from "fs";
 
 const create = (path) => {
   if (existsSync(path)) {
@@ -11,21 +18,33 @@ const create = (path) => {
   }
 };
 const copy = async (src, dest) => {
-    if (existsSync(src)) {
+  if (existsSync(src)) {
     const files = readdirSync(src, (err, data) => {
-        if (err) throw err;
-        return data ;
+      if (err) throw err;
+      return data;
     });
     mkdir(dest, (err) => {
-        if (err) throw new Error('FS operation failed');
+      if (err) throw new Error("FS operation failed");
     });
-    files.map(file => {
-        copyFile(path.join(src, file), path.join(dest, file), (err) => {
-           if (err) throw err;
-       });
+    files.map((file) => {
+      copyFile(path.join(src, file), path.join(dest, file), (err) => {
+        if (err) throw err;
+      });
     });
-   } else {
-     throw new Error('FS operation failed')
-   }
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
-export { create, copy };
+const rename = async (pathToFolder, oldFile, newFile) => {
+  const pathOldFile = path.join(pathToFolder, oldFile);
+  if (
+    existsSync(pathOldFile) &&
+    !existsSync(path.join(pathToFolder, newFile))
+  ) {
+    renameSync(pathOldFile, join(pathToFolder, newFile));
+  } else {
+    throw new Error("FS operation failed");
+  }
+};
+
+export { create, copy, rename };
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,8 @@
-const rename = async () => {
-    // Write your code here 
-};
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { rename } from "./files/modules.js";
 
-await rename();
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const pathToFileFolder = path.join(_dirname, "./files");
+rename(pathToFileFolder, 'wrongFilename.txt', 'properFilename.md');
