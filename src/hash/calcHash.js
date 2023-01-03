c4be66b022952940435cdb4bb760039eb4fa59d0
JMSBRN <zakhavai@gmail.com>
2023-01-03 14:32:47 +0300

    fix: upd_task_4_calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+ import crypto from 'crypto';
+ import { dirname, join } from 'path';
+ import { fileURLToPath } from 'url';
+ import { readFileSync } from 'fs';
+ import { calculateHash } from './modules.js';
 
-await calculateHash();
+ const _filename = fileURLToPath(import.meta.url);
+ const _dirname = dirname(_filename);
+ const pathToFile = join(_dirname, './files', 'fileToCalculateHashFor.txt');
+ const textFromFile = readFileSync(pathToFile, 'utf-8')
+ await calculateHash(textFromFile);
