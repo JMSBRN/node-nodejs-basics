efcf72108561c8de4266168f75bb740881f9ccb0
JMSBRN <zakhavai@gmail.com>
2023-01-04 14:17:12 +0300

    fix: upd_task_6_compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
-const compress = async () => {
-    // Write your code here 
-};
+import { createReadStream, createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import zlib from "zlib";
 
-await compress();
+const gzip = zlib.createGzip();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToFile = join(__dirname, "./files", "fileToCompress.txt");
+const pathToZipFile = join(__dirname, "./files", "archive.gz");
+
+const compress = () => {
+  const r = createReadStream(pathToFile);
+  const w = createWriteStream(pathToZipFile);
+  r.pipe(gzip).pipe(w);
+};
+compress();
