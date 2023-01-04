cad893f175cc86b4f9cbd24573c76572c6fca315
JMSBRN <zakhavai@gmail.com>
2023-01-04 17:18:18 +0300

    fix: upd_task_7_worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,17 @@
-fconst performCalculations = async () => {
-    // Write your code here
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { Worker } from "worker_threads";
+const pathToWorker = join(
+  dirname(fileURLToPath(import.meta.url)),
+  "./worker.js"
+);
+const performCalculations = async () => {
+  const worker = new Worker(pathToWorker, {
+    workerData: 12,
+  });
+  worker.on("message", (msg) => {
+    console.log("result from worker", msg);
+  });
 };
 
 await performCalculations();
