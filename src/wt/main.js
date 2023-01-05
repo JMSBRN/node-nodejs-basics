78d58845c1620dd68ac9b19d40f5e894d61b86c1
JMSBRN <zakhavai@gmail.com>
2023-01-05 16:10:13 +0300

    fix: upd_task_7_main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,10 +1,11 @@
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
-import { Worker } from "worker_threads";
+import { isMainThread, Worker, BroadcastChannel, workerData, parentPort } from "worker_threads";
 const pathToWorker = join(
   dirname(fileURLToPath(import.meta.url)),
   "./worker.js"
 );
+const bc = new BroadcastChannel('ch_1');
 const performCalculations = async () => {
   const worker = new Worker(pathToWorker, {
     workerData: 12,
@@ -14,4 +15,24 @@ const performCalculations = async () => {
   });
 };
 
-await performCalculations();
+// await performCalculations();
+ if(isMainThread) {
+  new Worker(pathToWorker);
+  const arr = [];
+  bc.onmessage = (msg) => {
+    for (let i = 0; i < msg.data; i++) {
+      const worker = new Worker(fileURLToPath(import.meta.url), {
+        workerData: i + 10,
+      });
+      worker.on('message', (msg) => {
+        arr.push(msg)
+      })
+    }
+  }
+  setTimeout(() => {
+    console.log(arr.sort((a, b) => a.data - b.data));
+    bc.close();
+  }, 1000);
+ } else {
+    parentPort.postMessage({status: workerData ? 'resolved' : 'error' , data: workerData || null });
+ }
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,10 +1,13 @@
-import { workerData, parentPort } from "worker_threads";
+import { workerData, parentPort, BroadcastChannel } from "worker_threads";
+import os from 'os';
 
+const bc = new BroadcastChannel('ch_1');
 // n should be received from main thread
 const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 const sendResult = () => {
-  parentPort.postMessage(nthFibonacci(workerData));
+  bc.postMessage(os.cpus().length);
+  //parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
